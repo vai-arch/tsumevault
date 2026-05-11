f5862cf6e895bc9bb5c92de6e8c9e859269017e9
vai-arch <victordiazcaballeroai@gmail.com>
2026-05-11 14:11:53 +0200

    update

--- subir_git.bat
+++ subir_git.bat
@@ -1,2 +1,4 @@
-git add -A && git commit -m "update" && git push
+sqlite3 tsumeVault.db "PRAGMA wal_checkpoint(TRUNCATE)"
 git add -f tsumeVault.db
+git add -A && git commit -m "update" && git push
+
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'tsumevault-v27';
+const CACHE_VERSION = 'tsumevault-v28';
 
 const STATIC_ASSETS = [
   '/tsumevault/tsumevault.html',
