02a67fd46c93f38bcccb127853585cb9c13cf0ca
vai-arch <victordiazcaballeroai@gmail.com>
2026-05-29 12:22:41 +0200

    review tab, stats modal, maturity metrics, db scripts

--- _bajar_BD_de_Hetzner.bat
+++ _bajar_BD_de_Hetzner.bat
@@ -1,2 +1,5 @@
 echo Bajando base de datos de Hetzner...
+ssh root@46.225.97.185 "systemctl stop tsumevault && sqlite3 /opt/tsumevault/tsumeVault.db 'PRAGMA wal_checkpoint(TRUNCATE);'"
 scp root@46.225.97.185:/opt/tsumevault/tsumeVault.db "%~dp0tsumeVault.db"
+ssh root@46.225.97.185 "systemctl start tsumevault"
+echo Hecho.
