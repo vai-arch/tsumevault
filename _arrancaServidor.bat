55a83f5c7d1c092f751c079204901367994116db
vai-arch <victordiazcaballeroai@gmail.com>
2026-08-27 12:55:38 +0200

    update

--- _arrancaServidor.bat
+++ _arrancaServidor.bat
@@ -1,5 +1,7 @@
 @echo off
 
+start "PLAYER_SAVE_SERVER" cmd /k "title PLAYER_SAVE_SERVER && python .\player\player_save_server.py"
+
 start "SAVE_SERVER" cmd /k "title SAVE_SERVER && python .\tsumevault_server.py"
 
 start "STATIC_SERVER" cmd /k "title STATIC_SERVER && npx serve .."
